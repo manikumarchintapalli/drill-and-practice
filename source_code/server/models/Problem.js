9a8de4fecbe4fd400216d239e3cf0ca5a1134b98
Manikumar Chintapalli <manikumarchintapali@gmail.com>
2025-05-01 20:12:55 -0400

    changes

--- source_code/server/models/Problem.js
+++ source_code/server/models/Problem.js
@@ -1,28 +1,7 @@
-// import mongoose from "mongoose";
-
-// const problemSchema = new mongoose.Schema({
-//   topic: { type: String, required: true },
-//   title: { type: String, required: true },
-//   description: String,
-//   options: [String],
-//   answerIndex: Number,
-//   difficulty: {
-//     type: String,
-//     enum: ["Easy", "Medium", "Hard"],
-//     default: "Medium",
-//   },
-// });
-
-// const Problem = mongoose.model("Problem", problemSchema);
-// export default Problem; 
 import mongoose from "mongoose";
 
 const problemSchema = new mongoose.Schema({
-  topic: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Topic", // ✅ reference Topic collection
-    required: true,
-  },
+  topic: { type: String, required: true },
   title: { type: String, required: true },
   description: String,
   options: [String],
